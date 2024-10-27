55e569eb0e63b7e39d78af300ca06657942bf764
steven-felim <stevenfelim04@gmail.com>
2024-10-27 21:58:40 +0700

    bikin view nomor 1

---
+++ src/view/View1.java
@@ -0,0 +1,9 @@
+package view;
+
+import javax.swing.*;
+
+public class View1 {
+    public void displayNomor1(String user) {
+        JOptionPane.showMessageDialog(null, user, "Data", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
