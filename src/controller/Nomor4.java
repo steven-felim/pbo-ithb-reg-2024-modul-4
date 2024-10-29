b01f182fda780b3aca95d62368e4439bddd465ee
steven-felim <stevenfelim04@gmail.com>
2024-10-29 22:51:52 +0700

    bikin nomor 4

---
+++ src/controller/Nomor4.java
@@ -0,0 +1,50 @@
+package controller;
+
+import model.classes.*;
+import view.View4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Nomor4 {
+    public static void printMhsTidakLulus(String kodeMk, List<Mahasiswa> mhs, ArrayList<MatkulAmbil> matkul) {
+        View4 view = new View4();
+        boolean foundMK = false;
+        int counterTidakLulus = 0, counterTotal = 0;
+        for (Mahasiswa m : mhs) {
+            double nilaiAkhir = 0;
+            if (m instanceof MahasiswaSarjana) {
+                for (MatkulAmbil mk : ((MahasiswaSarjana) m).getAmbil()) {
+                    if (mk.getAmbil().getKode().equalsIgnoreCase(kodeMk)) {
+                        foundMK = true;
+                        counterTotal += 1;
+                        nilaiAkhir = HitungNA.hitungNA(mk.getN1(), mk.getN2(), mk.getN3());
+                        if (nilaiAkhir < 56) {
+                            counterTidakLulus += 1;
+                        }
+                        break;
+                    }
+                }
+            }
+            if (m instanceof MahasiswaMagister) {
+                for (MatkulAmbil mk : ((MahasiswaMagister) m).getAmbil()) {
+                    if (mk.getAmbil().getKode().equalsIgnoreCase(kodeMk)) {
+                        foundMK = true;
+                        counterTotal += 1;
+                        nilaiAkhir = HitungNA.hitungNA(mk.getN1(), mk.getN2(), mk.getN3());
+                        if (nilaiAkhir < 56) {
+                            counterTidakLulus += 1;
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+
+        if (foundMK) {
+            view.displayNomor4(kodeMk, counterTidakLulus, counterTotal);
+        } else {
+            view.displayNomor4(kodeMk + " tidak ditemukan", 0, 0);
+        }
+    }
+}
---
+++ src/view/View4.java
@@ -0,0 +1,9 @@
+package view;
+
+import javax.swing.*;
+
+public class View4 {
+    public void displayNomor4(String matkul, int tidakLulus, int total) {
+        JOptionPane.showMessageDialog(null, matkul + "\nBanyak mahasiswa tidak lulus: " + tidakLulus + " dari " + total, "Nilai Akhir", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
