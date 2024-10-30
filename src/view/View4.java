cc78141831b45cd4ec1aefb8b9256ba6b415e03d
steven-felim <stevenfelim04@gmail.com>
2024-10-30 09:37:23 +0700

    update sedikit

--- src/view/View4.java
+++ src/view/View4.java
@@ -4,6 +4,6 @@ import javax.swing.*;
 
 public class View4 {
     public void displayNomor4(String matkul, int tidakLulus, int total) {
-        JOptionPane.showMessageDialog(null, matkul + "\nBanyak mahasiswa tidak lulus: " + tidakLulus + " dari " + total, "Nilai Akhir", JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, matkul + "\nBanyak mahasiswa tidak lulus: " + tidakLulus + " dari " + total, "Jumlah Mahasiswa Tidak Lulus", JOptionPane.INFORMATION_MESSAGE);
     }
 }
